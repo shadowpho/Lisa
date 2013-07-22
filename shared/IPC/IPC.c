77dccd96b04272abad0017a98146ec764ab4f988
george <shadowphoenix@debian.localdomain>
2013-07-22 01:45:05 -0700

    First shared library!

---
+++ shared/IPC/IPC.c
@@ -0,0 +1,28 @@
+#include "IPC.h"
+
+int start_com(int portNum)
+{
+    return -1;
+}
+
+int peek(int* mess, int* len)
+{
+    return -1;
+}
+
+int recieve(char* buff, int len)
+{
+    return -1;
+}
+
+int send(int program, int message, char* buff, int len)
+{
+    return -1;
+}
+
+float get_version()
+{
+    return IPC_version;
+}
+
+
---
+++ shared/IPC/IPC.h
@@ -0,0 +1,12 @@
+
+static float IPC_version = IPC_VERSION;
+
+int start_com(int listenNum);
+int peek(int* mess, int* len);
+int recieve(char* buff, int len);
+
+int send(int program, int message, char* buff, int len);
+
+float get_version();
+
+
