74e7f66f8a858f96f7d7d0bb2aedc4ef1b9b5aa8
george <shadowphoenix@debian.localdomain>
2013-07-22 23:21:58 -0700

    Test infrastructure

---
+++ shared/IPC/test/test_IPC.c
@@ -0,0 +1,27 @@
+#include "test_IPC.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int load_lib(char* path)
+{
+    return -1;
+}
+
+int resolve_syms()
+{
+    return -1;
+}
+
+int close_lib()
+{
+    return -1;
+}
+
+int main(int argv, char* argc[])
+{
+
+
+    return -1;
+}
---
+++ shared/IPC/test/test_IPC.h
@@ -0,0 +1,9 @@
+#include <dlfcn.h>
+
+
+
+int load_lib(char* path);
+
+int resolve_syms();
+
+int close_lib();
