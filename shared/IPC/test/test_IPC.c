af1bedda2dc8ca53a59e672ef6e1295ad8446218
george <shadowphoenix@debian.localdomain>
2013-07-26 00:30:35 -0700

    Added .exe files, library now loaded, all exe now

--- shared/IPC/test/test_IPC.c
+++ shared/IPC/test/test_IPC.c
@@ -3,10 +3,18 @@
 #include <stdlib.h>
 #include <assert.h>
 
+void static *test_handle;
 
 int load_lib(char* path)
 {
-    return -1;
+
+    test_handle = dlopen(IPC_LIBRARY_PATH,RTLD_LAZY|RTLD_LOCAL);
+    if(!test_handle)
+    {
+        printf("%s\n",dlerror());
+        return -1;
+    }
+    return 0;
 }
 
 int resolve_syms()
